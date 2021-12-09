cb12d9db3e5ff3c41fd21aabc1ded820bfc5ae23
Michael Wood <mnet@awoodfamily.com>
2021-12-09 16:37:01 -0700

    Update chessControl.c

--- chessControl.c
+++ chessControl.c
@@ -38,16 +38,46 @@
   currentState = update_board_st;                                              \
   nextState = NEXT;
 
-#define MSG_SIZE 4
-#define MSG_CURSOR SQUARE_DIM / 2, DISPLAY_HEIGHT / 2 - SQUARE_DIM / 2
 #define MSG_COLOR DISPLAY_WHITE
 #define MSG_BG_COLOR DISPLAY_BLACK
+
 #define GAME_OVER_MSG "GAME OVER"
+#define GAME_OVER_MSG_CURSOR SQUARE_DIM / 2, DISPLAY_HEIGHT / 2 - SQUARE_DIM / 2
+#define GAME_OVER_MSG_SIZE 4
+
+#define WIN_MSG "WINS"
+#define WIN_MSG_CURSOR SQUARE_DIM * 8 + 8, DISPLAY_HEIGHT / 2
+#define WIN_MSG_SIZE 2
+
+#define WHITE_WIN_MSG "WHITE"
+#define WHITE_WIN_MSG_CURSOR WIN_MSG_CURSOR - 16
+#define WHITE_WIN_MSG_SIZE 2
+
+#define BLACK_WIN_MSG "BLACK"
+#define BLACK_WIN_MSG_CURSOR WHITE_WIN_MSG_CURSOR
+#define BLACK_WIN_MSG_SIZE 2
+
+#define CHECK_MSG "CHECK!"
+#define CHECK_MSG_CURSOR WHITE_WIN_MSG_CURSOR
+#define CHECK_MSG_SIZE 2
+
+#define STALEMATE_MSG1 "STALE"
+#define STALEMATE_MSG2 "MATE"
+#define STALEMATE_MSG1_CURSOR WHITE_WIN_MSG_CURSOR
+#define STALEMATE_MSG2_CURSOR WIN_MSG_CURSOR
+#define STALEMATE_MSG1_SIZE 2
+#define STALEMATE_MSG2_SIZE 2
 
 #define PRINT_MSG(MSG)                                                         \
-  display_setCursor(MSG_CURSOR);                                               \
-  display_setTextSize(MSG_SIZE);                                               \
+  display_setCursor(MSG##_CURSOR);                                             \
+  display_setTextSize(MSG##_SIZE);                                             \
   display_setTextColorBg(MSG_COLOR, MSG_BG_COLOR);                             \
+  display_println(MSG);
+
+#define ERASE_MSG(MSG)                                                         \
+  display_setCursor(MSG##_CURSOR);                                             \
+  display_setTextSize(MSG##_SIZE);                                             \
+  display_setTextColor(MSG_BG_COLOR);                                          \
   display_println(MSG);
 
 // The different states of the state machine
@@ -76,8 +106,9 @@ static uint8_t piece_col;
 static uint8_t move_row;
 static uint8_t move_col;
 static board_move_list_t moves;
-static bool has_moves;
 static board_move_t current_move;
+static bool has_moves;
+static bool in_check;
 
 /* Helper functions */
 
@@ -101,6 +132,30 @@ static inline bool is_valid_move() {
       return true;
   }
   return false;
+}
+
+// Update the board by making a move
+static void make_move(board_move_t move) {
+  board_move_t move_simplified = board_simplify_move(move);
+
+  // remove piece where move begins
+  chessDisplay_displayPiece(
+      board.pieces[move_simplified.start_row][move_simplified.start_col],
+      move_simplified.start_row, move_simplified.start_col, ERASE);
+
+  // remove piece where move ends
+  chessDisplay_displayPiece(
+      board.pieces[move_simplified.end_row][move_simplified.end_col],
+      move_simplified.end_row, move_simplified.end_col, ERASE);
+
+  // apply move
+  board_apply_move(&board, move);
+
+  // draw piece where move ends
+  chessDisplay_displayPiece(
+      board.pieces[move_simplified.end_row][move_simplified.end_col],
+      move_simplified.end_row, move_simplified.end_col, DRAW);
+  // TODO - DRAW Castling/En Passant
 }
 
 // Debug function that prints the state as it is entered
@@ -164,29 +219,6 @@ static void debugStatePrint() {
   }
 }
 
-static void make_move(board_move_t move) {
-  board_move_t move_simplified = board_simplify_move(move);
-
-  // remove piece where move begins
-  chessDisplay_displayPiece(
-      board.pieces[move_simplified.start_row][move_simplified.start_col],
-      move_simplified.start_row, move_simplified.start_col, ERASE);
-
-  // remove piece where move ends
-  chessDisplay_displayPiece(
-      board.pieces[move_simplified.end_row][move_simplified.end_col],
-      move_simplified.end_row, move_simplified.end_col, ERASE);
-
-  // apply move
-  board_apply_move(&board, move);
-
-  // draw piece where move ends
-  chessDisplay_displayPiece(
-      board.pieces[move_simplified.end_row][move_simplified.end_col],
-      move_simplified.end_row, move_simplified.end_col, DRAW);
-  // TODO - DRAW Castling
-}
-
 // Inits the SM
 void chessControl_init() {
   currentState = init_st;
@@ -215,6 +247,7 @@ void chessControl_tick() {
     adc_counter = 0;
     has_moves = false;
     player_color = PIECE_COLOR_WHITE;
+    in_check = false;
     break;
 
   case start_game_st:
@@ -231,8 +264,13 @@ void chessControl_tick() {
 
   case verify_piece_st:
     if (is_valid_piece()) {
-      // TODO - Draw possible moves
       currentState = select_move_st;
+
+      // Highlight selected piece
+      chessDisplay_pieceChosen(board.pieces[piece_row][piece_col], piece_row,
+                               piece_col);
+
+      // TODO - Draw possible moves
     } else
       currentState = select_piece_st;
     break;
@@ -255,8 +293,16 @@ void chessControl_tick() {
         UPDATE_BOARD(ai_turn_st)
         async_ai_request_move(&board);
       }
-    } else
-      currentState = select_move_st;
+    } else {
+      // Check if user deselects piece
+      if (move_row == piece_row && move_col == piece_col) {
+        currentState = select_piece_st;
+        // Dehighlight piece
+        chessDisplay_displayPiece(board.pieces[piece_row][piece_col], piece_row,
+                                  piece_col, DRAW);
+      } else
+        currentState = select_move_st;
+    }
     break;
 
   case ai_turn_st:
@@ -277,11 +323,34 @@ void chessControl_tick() {
     break;
 
   case update_board_st:
-    if (minimax_is_game_over(&board)) {
-      currentState = game_over_st;
-      PRINT_MSG(GAME_OVER_MSG)
-    } else
-      currentState = nextState;
+    // Get board state to print messages
+    {
+      minimax_state_t state = minimax_get_board_state(&board);
+      if (state == MINIMAX_CHECKMATE || state == MINIMAX_DRAW) {
+        currentState = game_over_st;
+        PRINT_MSG(GAME_OVER_MSG)
+        if (state == MINIMAX_DRAW) {
+          PRINT_MSG(STALEMATE_MSG1)
+          PRINT_MSG(STALEMATE_MSG2)
+        } else {
+          if (board.last_player_is_white) {
+            PRINT_MSG(WHITE_WIN_MSG)
+          } else {
+            PRINT_MSG(BLACK_WIN_MSG)
+          }
+          PRINT_MSG(WIN_MSG)
+        }
+      } else {
+        if (!in_check && state == MINIMAX_CHECK) {
+          PRINT_MSG(CHECK_MSG)
+          in_check = true;
+        } else if (in_check && state != MINIMAX_CHECK) {
+          ERASE_MSG(CHECK_MSG)
+          in_check = false;
+        }
+        currentState = nextState;
+      }
+    }
     break;
 
   default:
@@ -307,7 +376,7 @@ void chessControl_tick() {
 
   case verify_piece_st:
     chessDisplay_touchScreencomputeBoardRowColumn(&piece_row, &piece_col);
-    printf("Piece location: %d %d\n", piece_row, piece_col);
+    // printf("Piece location: %d %d\n", piece_row, piece_col);
     break;
 
   case select_move_st:
@@ -315,7 +384,7 @@ void chessControl_tick() {
 
   case verify_move_st:
     chessDisplay_touchScreencomputeBoardRowColumn(&move_row, &move_col);
-    printf("Move location: %d %d\n", move_row, move_col);
+    // printf("Move location: %d %d\n", move_row, move_col);
     break;
 
   case ai_turn_st:
@@ -330,6 +399,21 @@ void chessControl_tick() {
 
   case update_board_st:
     make_move(current_move);
+    /*
+      // Draw updates
+      chessDisplay_displayPiece(board.pieces[move_row][move_col], move_row,
+                                move_col, ERASE);
+      chessDisplay_displayPiece(board.pieces[piece_row][piece_col], piece_row,
+                                piece_col, ERASE);
+      chessDisplay_displayPiece(board.pieces[piece_row][piece_col], move_row,
+                                move_col, DRAW);
+      // TODO - DRAW Castling
+
+      // Update board var
+      board_apply_move(&board,
+                       (board_move_t){piece_row, piece_col, move_row,
+      move_col});
+  */
     // Moves have updated
     has_moves = false;
     break;
